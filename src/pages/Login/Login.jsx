8e42f4e62d187954210cbba6d14cc60f2df3cc85
faheemk23 <faheemwork23@gmail.com>
2023-08-09 05:28:21 +0530

    refactor: unused code

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,8 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "./Login.css";
 
 export function Login() {
-  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
-    useAuth0();
+  const { loginWithRedirect } = useAuth0();
   return (
     <div className="login-container">
       <img
